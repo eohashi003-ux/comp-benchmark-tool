55abb8ee4e15d54e32ba407d239411473e0f662d
eohashi003-ux <ohashi.eisuke@gmail.com>
2026-05-15 22:07:39 +0100

    Updated AppLayout.tsx

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { colors, spacing } from "@/lib/designSystem";
+import { ui } from "@/lib/designSystem";
 
 export default function AppLayout({
   children,
@@ -6,8 +6,8 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`${colors.background} min-h-screen`}>
-      <div className={`${spacing.page} max-w-6xl mx-auto w-full px-4 sm:px-6`}>
+    <div className={`${ui.background} min-h-screen`}>
+      <div className="mx-auto max-w-7xl px-4 py-6">
         {children}
       </div>
     </div>
